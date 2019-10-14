ed5f015b91b4bfcdf77dab818b1fc51bcceffade
Andrew Capp <acapp909@gmail.com>
2019-10-14 23:17:29 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
 # Add  code here!
+require 'pry'
+def prime?(number)
+  number_range = []
+  i = 0
+  
+  if number > 1
+    p number_range = number.digits.sort
+
+  else
+
+
+  end
+
+  
+
+
+end
+
+
+
+p prime?(15)
